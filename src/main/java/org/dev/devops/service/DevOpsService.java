8aaff916176f81fce3fd7ad66edea5fa77f18a33
marvin <marvinsalcedo1999@hotmail.com>
2025-04-01 23:53:19 -0500

    * Modifications for ELK Stack

--- src/main/java/org/dev/devops/service/DevOpsService.java
+++ src/main/java/org/dev/devops/service/DevOpsService.java
@@ -1,12 +1,14 @@
 package org.dev.devops.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.dev.devops.models.MessageRequest;
 import org.dev.devops.models.MessageResponse;
 import org.dev.devops.security.jwt.JwtProvider;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+@Log4j2
 @Service
 @RequiredArgsConstructor
 public class DevOpsService {
@@ -15,6 +17,7 @@ public class DevOpsService {
 
     public Mono<MessageResponse> getResponse(MessageRequest request) {
         String message = "Hello " + request.getTo() + ", your message will be send";
+        log.info(message);
         MessageResponse response = MessageResponse.builder()
                 .message(message)
                 .build();
